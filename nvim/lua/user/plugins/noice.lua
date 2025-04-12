4163446036ec885991baa07fff8a0df01caaa31b
Jorge Reyes <rjorge99@hotmail.com>
2025-04-12 17:56:03 -0600

    Se hace limpieza en noice

--- nvim/lua/user/plugins/noice.lua
+++ nvim/lua/user/plugins/noice.lua
@@ -6,14 +6,19 @@ return {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
     },
-
     config = function()
         local noice = require("noice")
 
         noice.setup({
             routes = {
                 {
-                    -- avoid annoying message
+                    filter = {
+                        event = "notify",
+                        find = "nil",
+                    },
+                    opts = { skip = true },
+                },
+                {
                     filter = {
                         event = "notify",
                         find = "No information available",
@@ -42,13 +47,9 @@ return {
                     ["cmp.entry.get_documentation"] = true,
                 },
             },
-            -- you can enable a preset for easier configuration
             presets = {
-                bottom_search = true, -- use a classic bottom cmdline for search
-                command_palette = true, -- position the cmdline and popupmenu together
-                -- long_message_to_split = false, -- long messages will be sent to a split
-                -- inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                -- lsp_doc_border = false, -- add a border to hover docs and signature help
+                bottom_search = true,
+                command_palette = true,
             },
             messages = {
                 enabled = false,
