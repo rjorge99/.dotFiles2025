75bfb7dbaa78e7e2c3c9156d1be5e67512aed8a8
Jorge Reyes <rjorge99@hotmail.com>
2025-04-12 18:05:34 -0600

    Se hace limpieza de lspconfig.lua

--- nvim/lua/user/plugins/lsp/lspconfig.lua
+++ nvim/lua/user/plugins/lsp/lspconfig.lua
@@ -3,7 +3,6 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
-        -- { "antosha417/nvim-lsp-file-operations", config = true }, -- TODO: Check how it works
         "folke/neodev.nvim",
     },
     config = function()
@@ -11,7 +10,7 @@ return {
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
         require("neodev").setup({
             library = {
-                enabled = true, -- habilita la biblioteca de Neovim
+                enabled = true,
                 runtime = true,
                 types = true,
                 plugins = true,
@@ -25,8 +24,8 @@ return {
             opts.buffer = bufnr
 
             opts.desc = "[G]oto [R]eferences"
-            keymap.set("n", "gr", "<cmd>Lspsaga finder<CR>", opts) -- show definition, references
-            -- keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+            keymap.set("n", "gr", "<cmd>Lspsaga finder<CR>", opts)
+            -- keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
 
             vim.keymap.set("n", "<leader>fq", function()
                 local clients = vim.lsp.get_clients({ bufnr = 0 })
@@ -35,7 +34,7 @@ return {
                     return
                 end
 
-                local client = clients[1] -- Podrías iterar si querés algo más robusto
+                local client = clients[1]
                 local params = vim.lsp.util.make_position_params(0, client.offset_encoding or "utf-16")
 
                 vim.lsp.buf_request(0, "textDocument/references", params, function(err, result, _, _)
@@ -53,69 +52,55 @@ return {
             end, { desc = "LSP references to Quickfix" })
 
             opts.desc = "[G]oto peek [D]efinition"
-            keymap.set("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts) -- Peek definition
+            keymap.set("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts)
 
             opts.desc = "[G]oto [d]efinition"
-            keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+            keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
             opts.desc = "[G]oto [I]mplementation"
-            keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+            keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
             opts.desc = "[G]oto [T]ype Definition"
-            keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+            keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
             opts.desc = "See available [C]ode [A]ctions"
-            keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+            keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
 
             opts.desc = "[R]e[n]ame"
-            keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+            keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
             opts.desc = "Show line [D]iagnostics"
-            keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+            keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
 
             opts.desc = "Show line [d]iagnostics in floating window"
-            keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+            keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
             opts.desc = "Go to previous diagnostic"
-            keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
+            keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 
             opts.desc = "Go to next diagnostic"
-            keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+            keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
 
             opts.desc = "Show documentation for what is under cursor"
-            keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+            keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
 
             opts.desc = "Restart LSP"
-            keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+            keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
         end
 
-        -- used to enable autocompletion (assign to every lsp server config)
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
-        -- configure html server
         lspconfig["html"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
         })
 
-        -- configure typescript server with plugin
         lspconfig["ts_ls"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
             filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
-            -- init_options = {
-            --     plugins = {
-            --         {
-            --             name = '@vue/typescript-plugin',
-            --             location = vim.fn.stdpath 'data' ..
-            --                 '/mason/packages/vue-language-server/node_modules/@vue/language-server',
-            --             languages = { 'vue' },
-            --         },
-            --     },
-            -- },
         })
 
-        -- configure css server
         lspconfig["cssls"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
@@ -126,20 +111,17 @@ return {
             on_attach = on_attach,
         })
 
-        -- configure prisma orm server
         lspconfig["prismals"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
         })
 
-        -- configure graphql language server
         lspconfig["graphql"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
             filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
         })
 
-        -- configure vue server
         lspconfig["volar"].setup({
             on_attach = on_attach,
             capabilities = capabilities,
@@ -160,7 +142,6 @@ return {
             filetypes = { "sh" },
         })
 
-        -- configure docker compose server
         lspconfig["docker_compose_language_service"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
@@ -172,23 +153,16 @@ return {
             on_attach = on_attach,
             filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
         })
-        -- configure python server
+
         lspconfig["pyright"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
         })
 
-        -- configure yaml server
-        -- lspconfig["yamlls"].setup({
-        --     capabilities = capabilities,
-        --     on_attach = on_attach,
-        -- })
-
-        -- configure lua server (with special settings)
         lspconfig["lua_ls"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
-            settings = { -- custom settings for lua
+            settings = {
                 Lua = {
                     telemetry = { enable = false },
                     diagnostics = { globals = { "vim" } },
@@ -209,7 +183,7 @@ return {
             cmd = { "rustup", "run", "stable", "rust-analyzer" },
         })
 
-        -- Muestra visualmente los errores
+        -- Shows diagnostics in virtual_text
         vim.diagnostic.config({
             virtual_text = true,
             signs = {
