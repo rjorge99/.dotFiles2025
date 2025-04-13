702f1b821542c98c29d2dad8bce5f6288d2501e5
Jorge Reyes <rjorge99@hotmail.com>
2025-04-12 23:20:20 -0600

    Se quita de lua line tabnine

--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -54,15 +54,6 @@ return {
             options = {
                 theme = my_lualine_theme,
             },
-
-            tabline = {
-                lualine_a = {},
-                lualine_b = { "branch" },
-                lualine_c = { "filename" },
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {},
-            },
             sections = {
 
                 lualine_x = {
@@ -74,7 +65,6 @@ return {
                     { "encoding" },
                     { "fileformat" },
                     { "filetype" },
-                    { "tabnine" },
                 },
             },
         })
