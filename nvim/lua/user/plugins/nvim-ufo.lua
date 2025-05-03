cf838433ec00c15ae1252891c966b943570810cb
Jorge Reyes <rjorge99@hotmail.com>
2025-05-03 12:00:56 -0600

    Se configura nvim-ufo

--- nvim/lua/user/plugins/nvim-ufo.lua
+++ nvim/lua/user/plugins/nvim-ufo.lua
@@ -2,22 +2,10 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = { "kevinhwang91/promise-async" },
     config = function()
-        -- require("ufo").setup({
-        --     provider_selector = function(bufnr, filetype, buftype)
-        --         return { "treesitter", "indent" }
-        --     end,
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.foldingRange = {
-            dynamicRegistration = false,
-            lineFoldingOnly = true,
-        }
-        local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
-        for _, ls in ipairs(language_servers) do
-            require("lspconfig")[ls].setup({
-                capabilities = capabilities,
-                -- you can add other fields for setting up lsp server in this table
-            })
-        end
-        require("ufo").setup() -- })
+        require("ufo").setup({
+            provider_selector = function(bufnr, filetype, buftype)
+                return { "treesitter", "indent" }
+            end,
+        })
     end,
 }
