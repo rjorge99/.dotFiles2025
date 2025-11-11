d7c2da9ce803b50c3e8c35fd8d9a9bd5dbe80482
Jorge Reyes <rjorge99@hotmail.com>
2025-11-11 14:48:21 -0600

    Se formatea .wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,20 +10,19 @@ end
 --config.default_domain = 'WSL:Ubuntu'
 
 config.font = wezterm.font_with_fallback({ -- fonts
-	'MesloLGS Nerd Font Mono',
-	'Hack FC Ligatured'
-	"UbuntuMono Nerd Font Mono"	
+	"MesloLGS Nerd Font Mono",
+	"Hack FC Ligatured",
+	"UbuntuMono Nerd Font Mono",
 })
 
-config.enable_tab_bar = false 			-- enable/disable tabs
-config.font_size = 17 				-- font size
-config.window_background_opacity = 0.95 	-- opacity
-config.color_scheme = "Catppuccin Mocha" 	-- color scheme
-config.line_height = 1.1 			-- line height
-config.window_close_confirmation = "NeverPrompt"	-- No prompt on exit
+config.enable_tab_bar = false -- enable/disable tabs
+config.font_size = 17 -- font size
+config.window_background_opacity = 0.95 -- opacity
+config.color_scheme = "Catppuccin Mocha" -- color scheme
+config.line_height = 1.1 -- line height
+config.window_close_confirmation = "NeverPrompt" -- No prompt on exit
 
 -- config.window_decorations = "RESIZE" -- enable/disable window bar
 -- config.window_background_image = "/home/rjorge99/wallpapers/groot.jpg"
-
 
 return config
